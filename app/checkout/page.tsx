b8e02c8433c3558ac6b732cb4f1d4d32ce3c29d4
herbertcarnaubadesouza <herbertcarnaubadesouza@gmail.com>
2025-08-21 17:14:06 -0300

    remove use session

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,9 +1,6 @@
 'use client';
 import React from 'React';
-import { useSession } from 'next-auth/react';
 export default function LoginPage() {
-    const { data: session } = useSession();
-    console.log(session);
     return (
         <div>
             <h1>Se voce estiver vendo essa pagina, voce ta logado</h1>
